1cab22029b85461ed9a449071f6faefb283265fd
Cassie Koomjian <cassie@substantial.com>
2018-10-01 11:16:12 -0700

    Add babel presets

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-require("./main.css");
-require("./index.html");
+require("babel-runtime/regenerator")
+require("./main.css")
+require("./index.html")
 
-var a = () => {
-  console.log("Hello from the future!")
+var a = async(args) => {
+  const { a, b }  = args
+  await console.log("Hello from the future!", a, b)
+  console.log("Done")
 }
+
+a({ a: 1, b: 2 })
